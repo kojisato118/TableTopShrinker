dfd9063a813936eca694156d6e0dd0fd2d27d11b
kojisato118 <k.s.9190118@gmail.com>
2016-06-17 16:18:02 +0900

    上に移動させたときに画像を白くさせたり、Navigationbarを表示したりする

--- TableTopShrinker/ViewControllers/ShrinkerTableViewController.swift
+++ TableTopShrinker/ViewControllers/ShrinkerTableViewController.swift
@@ -11,20 +11,44 @@ import UIKit
 class ShrinkerTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var maskView: UIView! //imageViewを白くぼやけさせるためのView
     @IBOutlet weak var imageHeightConst: NSLayoutConstraint!
+    @IBOutlet weak var imageTopConst: NSLayoutConstraint!
+    @IBOutlet weak var topLayoutGuideView: UIView! //NavigationBarのBottomの位置を知るためのView
 
-    private let defaultImageHeight : CGFloat = 200.0
-
+    private let defaultImageHeight : CGFloat = 275.0
+    private let upperVelocity :CGFloat = 0.5 //imageTopConstを上にずらす時の速さ割合
+    private let barAlpha : CGFloat = 0.7 // Navigationbarの透明度
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.contentInset.top = defaultImageHeight
         self.imageHeightConst.constant = defaultImageHeight
+        self.imageTopConst.constant = 0
 
         self.tableView.delegate = self
         self.tableView.dataSource = self
         
-        print("\(self.tableView.contentOffset.y)")
+        self.title = "title"
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor(colorLiteralRed: 1, green: 1, blue: 1, alpha: 0.5)
+        self.navigationController?.navigationBar.translucent = true
+        self.navigationController?.navigationBar.alpha = barAlpha
+        // http://qiita.com/mochizukikotaro/items/a4405701dcc706fd643e
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+        self.setTitleAlpha(0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,7 +71,7 @@ class ShrinkerTableViewController: UIViewController, UITableViewDelegate, UITabl
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        cell.textLabel?.text = "\(indexPath.row)"
+        cell.textLabel?.text = "cell: \(indexPath.row)番目"
 
         return cell
     }
@@ -59,6 +83,40 @@ class ShrinkerTableViewController: UIViewController, UITableViewDelegate, UITabl
     
     // MARK: - UIScrollViewDelegate
     func scrollViewDidScroll(scrollView: UIScrollView) {
-        self.imageHeightConst.constant = self.tableView.contentOffset.y * -1
+        if self.tableView.contentOffset.y * -1 >= defaultImageHeight{
+            // TableViewをしたに引っ張った時にImageViewのサイズをその分上げる
+            self.maskView.hidden = true
+            self.imageHeightConst.constant = self.tableView.contentOffset.y * -1
+            
+            self.setTitleAlpha(0)
+        }else{
+            // TableViewを上にスクロールするとき
+            if self.tableView.contentOffset.y + self.topLayoutGuideView.frame.origin.y >= 0{
+                // TableViewのコンテンツがNavigationbarより上に来た時はNavigationbarを表示
+                self.navigationController!.navigationBar.setBackgroundImage(nil, forBarMetrics: .Default)
+                self.navigationController!.navigationBar.shadowImage = nil
+                
+                return
+            }
+            
+            self.navigationController!.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+            self.navigationController!.navigationBar.shadowImage = UIImage()
+            
+            // スクロール速度より若干遅めに画像が移動する
+            let diff = self.tableView.contentOffset.y + defaultImageHeight
+            self.maskView.hidden = false
+            self.imageTopConst.constant = diff * -1 * upperVelocity
+            
+            // 画像を白くぼやけさせる
+            let alpha = 1 - self.tableView.contentOffset.y * -1 / defaultImageHeight
+            self.maskView.alpha = alpha
+            self.setTitleAlpha(alpha)
+        }
+    }
+    
+    // MARK: - 
+    func setTitleAlpha(alpha : CGFloat){
+        let selectedAttributes = [NSForegroundColorAttributeName : UIColor(red:0, green:0, blue:0, alpha:alpha)]
+        self.navigationController?.navigationBar.titleTextAttributes = selectedAttributes
     }
 }
